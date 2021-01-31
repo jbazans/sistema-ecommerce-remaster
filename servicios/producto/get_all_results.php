c531d603c62e0a6e7b543557f15409debad2aa32
jbazans <jbazans@uni.pe>
2021-01-31 16:55:23 -0500

    Add files via upload

--- servicios/producto/get_all_results.php
+++ servicios/producto/get_all_results.php
@@ -18,6 +18,20 @@ while($row=mysqli_fetch_array($result)){
 	$datos[$i]=$obj;
 	$i++;
 }
+/*
+//Añadiendo mas resultados de otro tabla
+$sql="select * from nueva_tabla where campo_buscar LIKE '%$text%'";
+$result=mysqli_query($con,$sql);
+while($row=mysqli_fetch_array($result)){
+	$obj=new stdClass();
+	$obj->codpro=$row['codpro'];
+	$obj->nompro=$row['nompro'];
+	$obj->despro=$row['despro'];
+	$obj->prepro=$row['prepro'];
+	$obj->rutimapro=$row['rutimapro'];
+	$datos[$i]=$obj;
+	$i++;
+}*/
 $response->datos=$datos;
 
 mysqli_close($con);
